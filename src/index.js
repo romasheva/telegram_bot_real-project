b3cbb4ece50c4aecbc49c227ed1dab516d5e3293
Roman_Shevchenko <sheva18.rom@gmail.com>
2020-01-21 11:39:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,10 +14,30 @@ mongoose.connect(config.DB_URL, {
     .catch((err) => console.log(err))
 require('./models/film.model')
 const Film = mongoose.model('films')
-database.films.forEach(f => new Film(f).save().catch(e => console.log(e)))
+// database.films.forEach(f => new Film(f).save().catch(e => console.log(e)))
 
 const bot = new TelegramBot(config.TOKEN, {
     polling: true
+})
+bot.on('message', msg => {
+    console.log('Working', msg.from.first_name)
+    const chatId = helper.getChatId(msg)
+    switch (msg.text) {
+        case kb.home.favorite:
+            break
+        case kb.home.films:
+            bot.sendMessage(chatId, `Выберите жанр:`, {
+                reply_markup: {keyboard: keyboard.films}
+            })
+            break
+        case kb.home.cinemas:
+            break
+        case kb.back:
+            bot.sendMessage(chatId, `Что хотите посмотреть?:`, {
+                reply_markup: {keyboard: keyboard.home}
+            })
+            break
+    }
 })
 bot.on('message', msg => {
     console.log('Working', msg.from.first_name)
