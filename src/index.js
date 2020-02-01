ab2efa9a0351158f5a3dd038a1b19acd21d98565
Roman_Shevchenko <sheva18.rom@gmail.com>
2020-02-01 16:05:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -104,6 +104,33 @@ bot.onText(/\/f(.+)/, (msg, [source, match]) => {
         })
     })
 })
+bot.onText(/\/c(.+)/, (msg, [source, match]) => {
+    const cinemaUuid = helper.getItemUuid(source)
+    const chatId = helper.getChatId(msg)
+    Cinema.findOne({uuid: cinemaUuid}).then(cinema => {
+        bot.sendMessage(chatId, `Кинотеатр ${cinema.name}`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: cinema.name,
+                            url: cinema.url
+                        },
+                        {
+                            text: 'Показать на карте',
+                            callback_data: JSON.stringify(cinema.uuid)
+                        }
+                    ],
+                    [
+                        {
+                            text: 'Показать фильмы',
+                            callback_data: cinema.films
+                        }
+                    ]
+                ]
+        }
+    })
+})
 
 function sendFilmsByQuery(chatId, query) {
     Film.find(query).then(films => {
