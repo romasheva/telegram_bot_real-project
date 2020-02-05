54b01350ce986b8e1f4fc35eab646551258368ce
Roman_Shevchenko <sheva18.rom@gmail.com>
2020-02-05 22:15:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -249,9 +249,9 @@ function showFavouriteFilms(chatId, telegramId) {
                 Film.find({uuid: {'$in': user.films}}).then(films => {
                     let html
                     if (films.length) {
-                    html = films.map((f, i) => {
-                        return `<b>${i + 1}</b> ${f.name} - <b>${f.rate}</b> (/f${f.uuid})`
-                    }).join('\n')
+                        html = films.map((f, i) => {
+                            return `<b>${i + 1}</b> ${f.name} - <b>${f.rate}</b> (/f${f.uuid})`
+                        }).join('\n')
                     } else {
                       html = 'Вы пока ничего не добавили'
                     }
