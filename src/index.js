fa100cbbb25d6dde522707ca445f5d4a1bcd852a
Roman_Shevchenko <sheva18.rom@gmail.com>
2020-02-08 22:59:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -174,6 +174,8 @@ bot.on('callback_query', query => {
     }
     const { type } = data
     if (type === ACTION_TYPE.SHOW_CINEMAS_MAP) {
+        const {lat, lon} = data
+        bot.sendLocation(query.message.chat.id, lat, lon)
     }   else if (type === ACTION_TYPE.SHOW_CINEMAS) {
         sendCinemasByQuery(userId, {uuid: {'$in': data.cinemaUuids}})
     }   else if (type === ACTION_TYPE.TOOGLE_FAV_FILM) {
