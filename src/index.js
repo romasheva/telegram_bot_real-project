e0f780a484dcdb8e2a06986f0dfdb44052a2b923
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2021-04-30 21:08:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ mongoose.connect(config.DB_URL, {
     .catch((err) => console.log(err)) 
 require('./models/film.model') 
 require('./models/cinema.model')   
-require('./models/user.model') 
+require('./models/user.model')  
 const Film = mongoose.model('films')
 const Cinema = mongoose.model('cinemas') 
 const User = mongoose.model('users')
