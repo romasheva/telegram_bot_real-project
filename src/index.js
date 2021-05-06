bae8ed16725cd70453c38bdd47ec99043c7e3925
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2021-05-06 18:16:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')    
 const config = require('./config')            
-const helper = require('./helper')                   
+const helper = require('./helper')                    
 const geolib = require('geolib')             
 const _ = require('lodash')                       
 const mongoose = require('mongoose')              
