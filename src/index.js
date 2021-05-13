e8d8712b03aaebd993bf117c00d23f22bb786760
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2021-05-13 21:27:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const ACTION_TYPE = {
 const bot = new TelegramBot(config.TOKEN, { 
     polling: true  
 }) 
-bot.on('message', msg => {
+bot.on('message', msg => { 
     console.log('Working', msg.from.first_name)
     const chatId = helper.getChatId(msg)
     switch (msg.text) {
